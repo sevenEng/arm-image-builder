99cbc705f8c10b7fc427557df45671040e68fdb0
Qi LI <liqi0425@gmail.com>
2016-11-08 13:55:14 +0000

    more kernel options, smaller image size

--- u-boot.sh
+++ u-boot.sh
@@ -10,6 +10,8 @@ make CROSS_COMPILE=arm-linux-gnueabi- V=$V -j12
 
 cat > boot.uscr <<"EOF"
 setenv xen_addr_r 0x42e00000
+setenv bootargs "console=dtuart dtuart=/soc@01c00000/serial@01c28000 dom0_mem=512M,max:512M"
+
 load mmc 0 ${xen_addr_r} /boot/xen
 load mmc 0 ${fdt_addr_r} /boot/${fdtfile}
 load mmc 0 ${kernel_addr_r} /boot/vmlinuz
@@ -23,8 +25,8 @@ fdt set /chosen \#size-cells <1>
 fdt mknod /chosen module@0
 fdt set /chosen/module@0 compatible "xen,linux-zimage" "xen,multiboot-module"
 fdt set /chosen/module@0 reg <${kernel_addr_r} 0x${filesize} >
-fdt set /chosen/module@0 bootargs "modules=loop,squashfs,sd-mod,usb-storage clk_ignore_unused rootflags=size=128M"
-fdt set /chosen xen,xen-bootargs "conswitch=x dom0_mem=256M dtuart=/soc@01c00000/serial@01c28000"
+fdt set /chosen/module@0 bootargs "modules=loop,squashfs,sd-mod,usb-storage clk_ignore_unused rootflags=size=512M"
+fdt set /chosen xen,xen-bootargs "conswitch=x dom0_mem=512M dtuart=/soc@01c00000/serial@01c28000"
 
 load mmc 0 ${ramdisk_addr_r} /boot/initramfs-grsec
 bootz ${xen_addr_r} ${ramdisk_addr_r}:${filesize} ${fdt_addr_r}
--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
 export V=${V-1}
-export TARGET=${TARGET-Cubieboard2}
+export TARGET=${TARGET-Cubietruck}
 
 export TARGETlc=$(tr '[:upper:]' '[:lower:]' <<<"$TARGET")
 export DTB=src/linux-stable/arch/arm/boot/dts/${DTB-sun7i-a20-${TARGETlc}.dtb}
@@ -7,7 +7,7 @@ export DTB=src/linux-stable/arch/arm/boot/dts/${DTB-sun7i-a20-${TARGETlc}.dtb}
 export ALPINEV=3.4.0
 export ALPINETGZ=alpine-uboot-$ALPINEV-armhf.tar.gz
 
-export SDSIZE=${SDSIZE-32G}
+export SDSIZE=${SDSIZE-16G}
 export UBOOTBIN=${UBOOTBIN-src/u-boot/u-boot-sunxi-with-spl.bin}
 export ZIMAGE=${ZIMAGE-src/linux-stable/arch/arm/boot/zImage}
 
